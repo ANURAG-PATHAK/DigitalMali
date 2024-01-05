0808be176407424b2a1109b76e21f817e8e682a4
ANURAG-PATHAK <pathakanurag605@gmail.com>
2024-01-05 20:14:28 +0530

    Update email sender in stripeWebhookHandler

--- src/webhooks.ts
+++ src/webhooks.ts
@@ -99,7 +99,7 @@ export const stripeWebhookHandler = async (
     // send receipt
     try {
       const data = await resend.emails.send({
-        from: 'DigitalHippo <hello@joshtriedcoding.com>',
+        from: 'DigitalMali <noreply@anuragpathaks.live>',
         to: [user.email],
         subject:
           'Thanks for your order! This is your receipt.',
