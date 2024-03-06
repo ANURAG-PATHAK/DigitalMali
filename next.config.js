642d2aa0c39e2336ed6713a681294e8a405ab341
ANURAG-PATHAK <pathakanurag605@gmail.com>
2024-03-06 20:53:39 +0530

    Update user authentication logic to use bcrypt hashing algorithm

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
 			},
 			{
 				protocol: "https",
-				hostname: "digitalmali-production.up.railway.app",
+				hostname: "digitalmali.onrender.com/",
 			},
 		],
 	},
--- src/collections/Products/Products.ts
+++ src/collections/Products/Products.ts
@@ -1,87 +1,82 @@
 import {
   AfterChangeHook,
   BeforeChangeHook,
-} from 'payload/dist/collections/config/types'
-import { PRODUCT_CATEGORIES } from '../../config'
-import { Access, CollectionConfig } from 'payload/types'
-import { Product, User } from '../../payload-types'
-import { stripe } from '../../lib/stripe'
+} from "payload/dist/collections/config/types";
+import { PRODUCT_CATEGORIES } from "../../config";
+import { Access, CollectionConfig } from "payload/types";
+import { Product, User } from "../../payload-types";
+import { stripe } from "../../lib/stripe";
 
-const addUser: BeforeChangeHook<Product> = async ({
-  req,
-  data,
-}) => {
-  const user = req.user
+const addUser: BeforeChangeHook<Product> = async ({ req, data }) => {
+  const user = req.user;
 
-  return { ...data, user: user.id }
-}
+  return { ...data, user: user.id };
+};
 
-const syncUser: AfterChangeHook<Product> = async ({
-  req,
-  doc,
-}) => {
+const syncUser: AfterChangeHook<Product> = async ({ req, doc }) => {
   const fullUser = await req.payload.findByID({
-    collection: 'users',
+    collection: "users",
     id: req.user.id,
-  })
+  });
 
-  if (fullUser && typeof fullUser === 'object') {
-    const { products } = fullUser
+  if (fullUser && typeof fullUser === "object") {
+    const { products } = fullUser;
 
     const allIDs = [
       ...(products?.map((product) =>
-        typeof product === 'object' ? product.id : product
+        typeof product === "object" ? product.id : product
       ) || []),
-    ]
+    ];
 
     const createdProductIDs = allIDs.filter(
       (id, index) => allIDs.indexOf(id) === index
-    )
+    );
 
-    const dataToUpdate = [...createdProductIDs, doc.id]
+    const dataToUpdate = [...createdProductIDs, doc.id];
 
     await req.payload.update({
-      collection: 'users',
+      collection: "users",
       id: fullUser.id,
       data: {
         products: dataToUpdate,
       },
-    })
+    });
   }
-}
+};
 
 const isAdminOrHasAccess =
   (): Access =>
   ({ req: { user: _user } }) => {
-    const user = _user as User | undefined
+    const user = _user as User | undefined;
 
-    if (!user) return false
-    if (user.role === 'admin') return true
+    if (!user) return false;
+    if (user.role === "admin") return true;
 
-    const userProductIDs = (user.products || []).reduce<
-      Array<string>
-    >((acc, product) => {
-      if (!product) return acc
-      if (typeof product === 'string') {
-        acc.push(product)
-      } else {
-        acc.push(product.id)
-      }
+    const userProductIDs = (user.products || []).reduce<Array<string>>(
+      (acc, product) => {
+        if (!product) return acc;
+        if (typeof product === "string") {
+          acc.push(product);
+        } else {
+          acc.push(product.id);
+        }
 
-      return acc
-    }, [])
+        return acc;
+      },
+      []
+    );
 
     return {
       id: {
         in: userProductIDs,
       },
-    }
-  }
+    };
+  };
 
 export const Products: CollectionConfig = {
-  slug: 'products',
+  slug: "products",
   admin: {
-    useAsTitle: 'name',
+    useAsTitle: "name",
   },
   access: {
     read: isAdminOrHasAccess(),
@@ -93,50 +88,48 @@ export const Products: CollectionConfig = {
     beforeChange: [
       addUser,
       async (args) => {
-        if (args.operation === 'create') {
-          const data = args.data as Product
+        if (args.operation === "create") {
+          const data = args.data as Product;
 
-          const createdProduct =
-            await stripe.products.create({
-              name: data.name,
-              default_price_data: {
-                currency: 'USD',
-                unit_amount: Math.round(data.price * 100),
-              },
-            })
+          const createdProduct = await stripe.products.create({
+            name: data.name,
+            default_price_data: {
+              currency: "USD",
+              unit_amount: Math.round(data.price * 100),
+            },
+          });
 
           const updated: Product = {
             ...data,
             stripeId: createdProduct.id,
             priceId: createdProduct.default_price as string,
-          }
+          };
 
-          return updated
-        } else if (args.operation === 'update') {
-          const data = args.data as Product
+          return updated;
+        } else if (args.operation === "update") {
+          const data = args.data as Product;
 
-          const updatedProduct =
-            await stripe.products.update(data.stripeId!, {
-              name: data.name,
-              default_price: data.priceId!,
-            })
+          const updatedProduct = await stripe.products.update(data.stripeId!, {
+            name: data.name,
+            default_price: data.priceId!,
+          });
 
           const updated: Product = {
             ...data,
             stripeId: updatedProduct.id,
             priceId: updatedProduct.default_price as string,
-          }
+          };
 
-          return updated
+          return updated;
         }
       },
     ],
   },
   fields: [
     {
-      name: 'user',
-      type: 'relationship',
-      relationTo: 'users',
+      name: "user",
+      type: "relationship",
+      relationTo: "users",
       required: true,
       hasMany: false,
       admin: {
@@ -144,109 +137,107 @@ export const Products: CollectionConfig = {
       },
     },
     {
-      name: 'name',
-      label: 'Name',
-      type: 'text',
+      name: "name",
+      label: "Name",
+      type: "text",
       required: true,
     },
     {
-      name: 'description',
-      type: 'textarea',
-      label: 'Product details',
+      name: "description",
+      type: "textarea",
+      label: "Product details",
     },
     {
-      name: 'price',
-      label: 'Price in USD',
+      name: "price",
+      label: "Price in USD",
       min: 0,
       max: 1000,
-      type: 'number',
+      type: "number",
       required: true,
     },
     {
-      name: 'category',
-      label: 'Category',
-      type: 'select',
-      options: PRODUCT_CATEGORIES.map(
-        ({ label, value }) => ({ label, value })
-      ),
+      name: "category",
+      label: "Category",
+      type: "select",
+      options: PRODUCT_CATEGORIES.map(({ label, value }) => ({ label, value })),
       required: true,
     },
     {
-      name: 'product_files',
-      label: 'Product file(s)',
-      type: 'relationship',
+      name: "product_files",
+      label: "Product file(s)",
+      type: "relationship",
       required: true,
-      relationTo: 'product_files',
+      relationTo: "product_files",
       hasMany: false,
     },
     {
-      name: 'approvedForSale',
-      label: 'Product Status',
-      type: 'select',
-      defaultValue: 'pending',
+      name: "approvedForSale",
+      label: "Product Status",
+      type: "select",
+      defaultValue: "pending",
       access: {
-        create: ({ req }) => req.user.role === 'admin',
-        read: ({ req }) => req.user.role === 'admin',
-        update: ({ req }) => req.user.role === 'admin',
+        create: ({ req }) => req.user.role === "admin",
+        read: ({ req }) => req.user.role === "admin",
+        update: ({ req }) => req.user.role === "admin",
       },
       options: [
         {
-          label: 'Pending verification',
-          value: 'pending',
+          label: "Pending verification",
+          value: "pending",
         },
         {
-          label: 'Approved',
-          value: 'approved',
+          label: "Approved",
+          value: "approved",
         },
         {
-          label: 'Denied',
-          value: 'denied',
+          label: "Denied",
+          value: "denied",
         },
       ],
     },
     {
-      name: 'priceId',
+      name: "priceId",
       access: {
         create: () => false,
         read: () => false,
         update: () => false,
       },
-      type: 'text',
+      type: "text",
       admin: {
         hidden: true,
       },
     },
     {
-      name: 'stripeId',
+      name: "stripeId",
       access: {
         create: () => false,
         read: () => false,
         update: () => false,
       },
-      type: 'text',
+      type: "text",
       admin: {
         hidden: true,
       },
     },
     {
-      name: 'images',
-      type: 'array',
-      label: 'Product images',
+      name: "images",
+      type: "array",
+      label: "Product images",
       minRows: 1,
       maxRows: 4,
       required: true,
       labels: {
-        singular: 'Image',
-        plural: 'Images',
+        singular: "Image",
+        plural: "Images",
       },
       fields: [
         {
-          name: 'image',
-          type: 'upload',
-          relationTo: 'media',
+          name: "image",
+          type: "upload",
+          relationTo: "media",
           required: true,
         },
       ],
     },
   ],
-}
+};
